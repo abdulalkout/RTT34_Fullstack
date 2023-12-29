a5384a51ea4a456dc23953c12b462b81fe71997c
abdulalkout <abdul.alkout@gmail.com>
2023-12-29 10:00:48 -0600

    styling the footer section

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,41 +1,42 @@
 // Dre
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <footer className="Footer">
-    <div>
-    <h2>PerScholas Instructors</h2>
-    <li>Teo Marcelo</li>
-    <li>Jordan Wofford</li>
-    <li>Dylan Comeau</li>
-    <li>Victor Vasquez</li>
-    </div>
-    <div>
-    <h2>The Developers</h2>
-    <li>Abdul Alkout</li>
-    <li>Jorge Robayo</li>
-    <li>Deandre Byrd</li>
-    <li>Mouloud Chabouni</li>
-    <li>Nurredyn Ballout</li>
-    </div>
-    <div>
-    <h2>About</h2>
-    <p>
-      Perscholas empowers students by offering comprehensive 
-      <br></br>industry-aligned courses that cultivate <br></br>
-      practical skills and foster a collaborative <br></br>
-      learning environment, preparing graduates <br></br>
-      for success in the ever-evolving tech landscape.
-
-
-
-      
-    </p>
-    </div>
+    <footer className="footer">
+      <div className="instructors-div">
+        <h2>PerScholas Instructors</h2>
+        <ul>
+          <li>Teo Marcelo</li>
+          <li>Jordan Wofford</li>
+          <li>Dylan Comeau</li>
+          <li>Victor Vasquez</li>
+        </ul>
+      </div>
+      <div className="instructors-div">
+        <h2>The Developers</h2>
+        <ul>
+          <li>Abdul Alkout</li>
+          <li>Jorge Robayo</li>
+          <li>Deandre Byrd</li>
+          <li>Mouloud Chabouni</li>
+          <li>Nurredyn Ballout</li>
+          <li>Affaf</li>
+          <li>Carmen</li>
+        </ul>
+      </div>
+      <div className="about-div">
+        <h2>About</h2>
+        <p>
+          Perscholas empowers students by offering comprehensive
+          industry-aligned courses that cultivate practical skills and foster a
+          collaborative learning environment, preparing graduates for success in
+          the ever-evolving tech landscape.
+        </p>
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;

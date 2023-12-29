c495315271174d53db3b385f813f65c09d595b95
abdulalkout <abdul.alkout@gmail.com>
2023-12-29 10:22:35 -0600

    adding logo

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,39 +1,43 @@
 // Dre
 import React from "react";
 import "./Footer.css";
+import logo2 from "../../images/logo/logo.jpg";
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="instructors-div">
-        <h2>PerScholas Instructors</h2>
-        <ul>
-          <li>Teo Marcelo</li>
-          <li>Jordan Wofford</li>
-          <li>Dylan Comeau</li>
-          <li>Victor Vasquez</li>
-        </ul>
-      </div>
-      <div className="instructors-div">
-        <h2>The Developers</h2>
-        <ul>
-          <li>Abdul Alkout</li>
-          <li>Jorge Robayo</li>
-          <li>Deandre Byrd</li>
-          <li>Mouloud Chabouni</li>
-          <li>Nurredyn Ballout</li>
-          <li>Affaf</li>
-          <li>Carmen</li>
-        </ul>
-      </div>
-      <div className="about-div">
-        <h2>About</h2>
-        <p>
-          Perscholas empowers students by offering comprehensive
-          industry-aligned courses that cultivate practical skills and foster a
-          collaborative learning environment, preparing graduates for success in
-          the ever-evolving tech landscape.
-        </p>
+      {/* <img src={logo2} /> */}
+      <div className="footer-info">
+        <div className="instructors-div">
+          <h2>PerScholas Instructors</h2>
+          <ul>
+            <li>Teo Marcelo</li>
+            <li>Jordan Wofford</li>
+            <li>Dylan Comeau</li>
+            <li>Victor Vasquez</li>
+          </ul>
+        </div>
+        <div className="instructors-div">
+          <h2>The Developers</h2>
+          <ul>
+            <li>Abdul Alkout</li>
+            <li>Jorge Robayo</li>
+            <li>Deandre Byrd</li>
+            <li>Mouloud Chabouni</li>
+            <li>Nurredyn Ballout</li>
+            <li>Affaf Taouti</li>
+            <li>Carmen</li>
+          </ul>
+        </div>
+        <div className="about-div">
+          <h2>About</h2>
+          <p>
+            Perscholas empowers students by offering comprehensive
+            industry-aligned courses that cultivate practical skills and foster
+            a collaborative learning environment, preparing graduates for
+            success in the ever-evolving tech landscape.
+          </p>
+        </div>
       </div>
     </footer>
   );
--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
+import logo from "../../images/logo/Damajc.png";
 
 function NavBar() {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -11,17 +12,19 @@ function NavBar() {
   return (
     <div className="sticky-nav">
       <div className="navbar">
-        <img
-          className="logo"
-          src="https://cdn2.abiresearch.com/static/company_logos/3471_Control4.webp"
-          alt="Logo"
-        />
+        <img className="logo" src={logo} alt="Logo" />
         <div className={`pageLinks ${isMobileMenuOpen ? "active" : ""}`}>
           <Link to="/" className="nav-links">
             <h3>Home</h3>
           </Link>
-          <a className="nav-links"> <h3>API's</h3> </a>
-          <a className="nav-links"> <h3>Stories</h3> </a>
+          <a className="nav-links">
+            {" "}
+            <h3>API's</h3>{" "}
+          </a>
+          <a className="nav-links">
+            {" "}
+            <h3>Stories</h3>{" "}
+          </a>
           <Link to="/about" className="nav-links">
             <h3>About</h3>
           </Link>

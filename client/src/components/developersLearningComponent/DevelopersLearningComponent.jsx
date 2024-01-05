40f81c0414887de1f4b163d717e7a358913d7619
jorge robayo <jorgerobmor@gmail.com>
2024-01-04 22:24:47 -0500

    changes in css homepage

--- client/src/components/developersLearningComponent/DevelopersLearningComponent.jsx
+++ client/src/components/developersLearningComponent/DevelopersLearningComponent.jsx
@@ -4,22 +4,24 @@ import { Link } from "react-router-dom";
 
 function DevelopersLearningComponent() {
   return (
-    <div className="developers-learning-component">
-      <p className="developer-learning-title">Developers cooperation</p>
-      <h4  className="developer-learning-discription">
-        In this section you will find cool small projects and solving small
-        coding problems
-      </h4>
-      <img
-        className="developer-learning-image"
-        src="https://cdn-blog.scalablepath.com/uploads/2018/05/designers-developers-collaboration-tools-1.png"
-      />
-      <br />
-      <button className="developer-learning-button">
-        <Link className="developer-learning-link" to="/cooperation">
-           <h4>Checkout the cool codes</h4>
-        </Link>
-      </button>
+    <div>
+      <div className="developers-learning-component">
+        <h1 className="developer-learning-title">Developers cooperation</h1>
+        <h3 className="developer-learning-discription">
+          In this section you will find cool small projects and solving small
+          coding problems
+        </h3>
+        <img
+          className="developer-learning-image"
+          src="https://cdn-blog.scalablepath.com/uploads/2018/05/designers-developers-collaboration-tools-1.png"
+        />
+        <br />
+        <button className="developer-learning-button">
+          <Link className="developer-learning-link" to="/cooperation">
+            <h4>Checkout the cool codes</h4>
+          </Link>
+        </button>
+      </div>
     </div>
   );
 }
